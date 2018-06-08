3bcb3ef1b4d5f1e66d81b9e55a141c643169b9af
DESKTOP-9PBV0VJ\piedistrict <jinimol@piedistrict.co.in>
2018-06-08 18:27:23 +0530

    Shopping program completed.
    Shopping program completed.

---
+++ shoppingExpense.js
@@ -0,0 +1,28 @@
+ let shoppingExpense = (details) => {
+	console.log("inside");
+	let total = 0;
+	details.map((value,id) => {
+		/* To calculate total amount*/
+		 total += parseInt(value.qty) * parseInt(value.price);
+	});
+	console.log("Total Expense is ",total)
+	
+};
+
+let details = [
+				{'name' : 'egg','qty':2,'price' :10},
+				{'name' : 'Chocolates','qty':10,'price' :60},
+				{'name' : 'pen','qty':1,'price' :10},
+				{'name' : 'bread','qty':1,'price' :30},
+				{'name' : 'jam','qty':1,'price' :25},
+			];
+let extDetails = [
+	{'name' : 'milk','qty':1,'price' :25},
+	{'name' : 'pencil','qty':2,'price' :5},
+	{'name' : 'biscuit','qty':1,'price' :20},
+	{'name' : 'toothpaste','qty':1,'price' :30},
+	{'name' : 'icecream','qty':10,'price' :15},
+];
+shoppingExpense(details);
+/* To check with different data*/
+//shoppingExpense(extDetails);
