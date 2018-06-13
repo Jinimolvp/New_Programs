e6dc07a57f73059f22e0bd69be2e44942f99bb99
DESKTOP-9PBV0VJ\piedistrict <jinimol@piedistrict.co.in>
2018-06-13 16:22:19 +0530

    Quiz prg started

---
+++ onlineQuiz.js
@@ -0,0 +1,28 @@
+
+let quize = [
+				{'question' : 'Who is selected as the 14th President of India?',
+				'options':[{'A':'Ram Nath Kovind'},
+				           {'B':'Suresh Kumar'}
+				          ]
+				},
+				{'question' : 'The 2018 Asia Cup has been relocated from India to which country?',
+				'options':[{'A':'Srilanka'},
+				           {'B':'UAE'}
+				          ]
+				},
+				{'question' : 'International No Diet Day was observed on __________.',
+				'options':[{'A':'May 7th'},
+				           {'B':'May 6th'}
+				          ]
+				},
+				{'question' : '	"OS" computer abbreviation usually means ?',
+				'options':[{'A':'Operating System'},
+				           {'B':'Order of Significance'}
+				          ]
+				},
+				{'question' : '".MOV" extension refers usually to what kind of file?',
+				'options':[{'A':'Animation/movie file'},
+				           {'B':'Image file'}
+				          ]
+				},
+			];
